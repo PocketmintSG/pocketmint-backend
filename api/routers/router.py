a2afba9b99b958eb66e56260b66578f1b5dc2e95
Mai Ting Kai <wheatgrass.mc@gmail.com>
2023-07-26 16:31:53 +0800

    Add route to upload images

--- api/routers/router.py
+++ api/routers/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from api.routers import auth
+from api.routers import auth, utils
 
 api_router = APIRouter()
 api_router.include_router(auth.router, tags=["Authentication"])
+api_router.include_router(utils.router, tags=["Utils"])
